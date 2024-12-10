31cbe39f37ce626ca801adfc9c6692f15faaa189
Arif Adi Nugroho <arifadinugroho86@gmail.com>
2024-12-10 21:05:02 +0700

    (feat): add

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,48 @@ use App\Http\Controllers\userLogin;
 use App\Http\Controllers\imageProfil;
 use App\Http\Controllers\penelusuran;
 use App\Http\Controllers\profilUtama;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\GoogleAuthController;
+use App\Http\Controllers\Auth\LupaPasswordController;
+use App\Http\Controllers\Auth\UbahPasswordController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
     return view ('welcome');
 });
+
+Route::middleware('guest')->prefix('auth')->group(function () {
+    Route::get('register', [RegisterController::class, 'create'])
+        ->name('register');
+
+    Route::post('register', [RegisterController::class, 'store']);
+
+    Route::get('login', [LoginController::class, 'create'])
+        ->name('login');
+
+    Route::post('login', [LoginController::class, 'store']);
+
+    Route::get('google', [GoogleAuthController::class, 'redirectToGoogle'])->name('login.google');
+
+    Route::get('google/callback', [GoogleAuthController::class, 'handleGoogleCallback']);
+
+    Route::get('lupa-password', [LupaPasswordController::class, 'create'])
+        ->name('password.request');
+
+    Route::post('lupa-password', [LupaPasswordController::class, 'store'])
+        ->name('password.email');
+
+    Route::get('ubah-password/{token}', [UbahPasswordController::class, 'create'])
+        ->name('password.reset');
+
+    Route::post('ubah-password', [UbahPasswordController::class, 'store'])
+        ->name('password.store');
+});
+
+Route::middleware('auth')->post('/auth/logout', [LogoutController::class, 'destroy'])->name('logout');
 
 Route::get('/dashboard', [dashboard::class, 'index']);
 Route::get('/kategori/{id}', [kategori::class, 'show'])->name('kategori.show');
